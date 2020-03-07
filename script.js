749102dd3be5bfff3f5241a0895fd3f74029c779
kzhub <55772750+kzhub@users.noreply.github.com>
2020-03-07 18:50:13 +0900

    文字間、a色変更

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ function whatssite(){
   open.style.display="block";
   let open2=document.getElementById("mask");
   open2.style.display="block";
+}
 
-
-
+function close(){
+  let cl=document.getElementById("modar");
+  cl.style.display="none";
+  let cp=document.getElementById("mask");
+  cp.style.display="none";
 }
